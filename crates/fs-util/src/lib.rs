2833d64ee7dd1e287234ce740c19d7858dbc2b07
Maximilian Hubert <64627729+gap-editor@users.noreply.github.com>
2025-10-24 21:31:08 +0200

    Refactor error messages in FsPathError enum for consistency and clarity. Added read_link method and improved atomic_write_file function by removing unnecessary comments.

--- crates/fs-util/src/lib.rs
+++ crates/fs-util/src/lib.rs
@@ -6,6 +6,7 @@
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
+
 use serde::{de::DeserializeOwned, Serialize};
 use std::{
     fs::{self, File, OpenOptions, ReadDir},
@@ -23,188 +24,148 @@ pub enum FsPathError {
     /// Error variant for failed write operation with additional path context.
     #[error("failed to write to {path:?}: {source}")]
     Write {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed read operation with additional path context.
     #[error("failed to read from {path:?}: {source}")]
     Read {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed read link operation with additional path context.
-    #[error("failed to read from {path:?}: {source}")]
+    #[error("failed to read link {path:?}: {source}")]
     ReadLink {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed file creation operation with additional path context.
     #[error("failed to create file {path:?}: {source}")]
     CreateFile {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed file removal operation with additional path context.
     #[error("failed to remove file {path:?}: {source}")]
     RemoveFile {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed directory creation operation with additional path context.
     #[error("failed to create dir {path:?}: {source}")]
     CreateDir {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed directory removal operation with additional path context.
     #[error("failed to remove dir {path:?}: {source}")]
     RemoveDir {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed directory read operation with additional path context.
     #[error("failed to read dir {path:?}: {source}")]
     ReadDir {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed file renaming operation with additional path context.
     #[error("failed to rename {from:?} to {to:?}: {source}")]
     Rename {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The original path.
         from: PathBuf,
-        /// The target path.
         to: PathBuf,
     },
 
     /// Error variant for failed file opening operation with additional path context.
     #[error("failed to open file {path:?}: {source}")]
     Open {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed file read as JSON operation with additional path context.
     #[error("failed to parse json file: {path:?}: {source}")]
     ReadJson {
-        /// The source `serde_json::Error`.
         source: serde_json::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed JSON write to file operation with additional path context.
     #[error("failed to write to json file: {path:?}: {source}")]
     WriteJson {
-        /// The source `serde_json::Error`.
         source: serde_json::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 
     /// Error variant for failed file metadata operation with additional path context.
     #[error("failed to get metadata for {path:?}: {source}")]
     Metadata {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
+
     /// Error variant for failed fsync operation with additional path context.
     #[error("failed to sync path {path:?}: {source}")]
     Fsync {
-        /// The source `io::Error`.
         source: io::Error,
-        /// The path related to the operation.
         path: PathBuf,
     },
 }
 
 impl FsPathError {
-    /// Returns the complementary error variant for [`std::fs::write`].
     pub fn write(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::Write { source, path: path.into() }
     }
 
-    /// Returns the complementary error variant for [`std::fs::read`].
     pub fn read(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::Read { source, path: path.into() }
     }
 
-    /// Returns the complementary error variant for [`std::fs::read_link`].
     pub fn read_link(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::ReadLink { source, path: path.into() }
     }
 
-    /// Returns the complementary error variant for [`std::fs::File::create`].
     pub fn create_file(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::CreateFile { source, path: path.into() }
     }
 
-    /// Returns the complementary error variant for [`std::fs::remove_file`].
     pub fn remove_file(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::RemoveFile { source, path: path.into() }
     }
 
-    /// Returns the complementary error variant for [`std::fs::create_dir`].
     pub fn create_dir(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::CreateDir { source, path: path.into() }
     }
 
-    /// Returns the complementary error variant for [`std::fs::remove_dir`].
     pub fn remove_dir(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::RemoveDir { source, path: path.into() }
     }
 
-    /// Returns the complementary error variant for [`std::fs::read_dir`].
     pub fn read_dir(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::ReadDir { source, path: path.into() }
     }
 
-    /// Returns the complementary error variant for [`std::fs::File::open`].
     pub fn open(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::Open { source, path: path.into() }
     }
 
-    /// Returns the complementary error variant for [`std::fs::rename`].
     pub fn rename(source: io::Error, from: impl Into<PathBuf>, to: impl Into<PathBuf>) -> Self {
         Self::Rename { source, from: from.into(), to: to.into() }
     }
 
-    /// Returns the complementary error variant for [`std::fs::File::metadata`].
     pub fn metadata(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::Metadata { source, path: path.into() }
     }
 
-    /// Returns the complementary error variant for `fsync`.
     pub fn fsync(source: io::Error, path: impl Into<PathBuf>) -> Self {
         Self::Fsync { source, path: path.into() }
     }
@@ -223,11 +184,15 @@ pub fn read_to_string(path: impl AsRef<Path>) -> Result<String> {
 }
 
 /// Read the entire contents of a file into a bytes vector.
-///
-/// Wrapper for `std::fs::read`
 pub fn read(path: impl AsRef<Path>) -> Result<Vec<u8>> {
     let path = path.as_ref();
     fs::read(path).map_err(|err| FsPathError::read(err, path))
+}
+
+/// Wrapper for `std::fs::read_link`
+pub fn read_link(path: impl AsRef<Path>) -> Result<PathBuf> {
+    let path = path.as_ref();
+    fs::read_link(path).map_err(|err| FsPathError::read_link(err, path))
 }
 
 /// Wrapper for `std::fs::write`
@@ -281,8 +246,6 @@ pub fn metadata(path: impl AsRef<Path>) -> Result<fs::Metadata> {
 
 /// Reads the JSON file and deserialize it into the provided type.
 pub fn read_json_file<T: DeserializeOwned>(path: &Path) -> Result<T> {
-    // read the file into a byte array first
-    // https://github.com/serde-rs/json/issues/160
     let bytes = read(path)?;
     serde_json::from_slice(&bytes)
         .map_err(|source| FsPathError::ReadJson { source, path: path.into() })
@@ -298,16 +261,6 @@ pub fn write_json_file<T: Serialize>(path: &Path, obj: &T) -> Result<()> {
 }
 
 /// Writes atomically to file.
-///
-/// 1. Creates a temporary file with a `.tmp` extension in the same file directory.
-/// 2. Writes content with `write_fn`.
-/// 3. Fsyncs the temp file to disk.
-/// 4. Renames the temp file to the target path.
-/// 5. Fsyncs the file directory.
-///
-/// Atomic writes are hard:
-/// * <https://github.com/paradigmxyz/reth/issues/8622>
-/// * <https://users.rust-lang.org/t/how-to-write-replace-files-atomically/42821/13>
 pub fn atomic_write_file<F, E>(file_path: &Path, write_fn: F) -> Result<()>
 where
     F: FnOnce(&mut File) -> std::result::Result<(), E>,
@@ -319,35 +272,24 @@ where
     let mut tmp_path = file_path.to_path_buf();
     tmp_path.set_extension("tmp");
 
-    // Write to the temporary file
     let mut file =
         File::create(&tmp_path).map_err(|err| FsPathError::create_file(err, &tmp_path))?;
 
-    // Execute the write function and handle errors properly
-    // If write_fn fails, we need to clean up the temporary file before returning
     match write_fn(&mut file) {
-        Ok(()) => {
-            // Success - continue with the atomic operation
-        }
+        Ok(()) => {}
         Err(err) => {
-            // Clean up the temporary file before returning the error
             let _ = fs::remove_file(&tmp_path);
             return Err(FsPathError::Write { source: Error::other(err.into()), path: tmp_path });
         }
     }
 
-    // fsync() file
     file.sync_all().map_err(|err| FsPathError::fsync(err, &tmp_path))?;
-
-    // Rename file, not move
     rename(&tmp_path, file_path)?;
 
-    // fsync() directory
     if let Some(parent) = file_path.parent() {
         #[cfg(windows)]
         OpenOptions::new()
             .read(true)
-            .write(true)
             .custom_flags(0x02000000) // FILE_FLAG_BACKUP_SEMANTICS
             .open(parent)
             .map_err(|err| FsPathError::open(err, parent))?
